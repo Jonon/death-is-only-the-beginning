7853c8689b0cdde562dab08dcb7ecdbb9cb9461a
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-05-28 22:41:21 +0200

    Add player movement left and right

--- Death Is Only The Beginning/Assets/Scripts/PlayerMovement.cs
+++ Death Is Only The Beginning/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   private Rigidbody2D body;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private void Awake() {
+       body = GetComponent<Rigidbody2D>();
+   }
+
+   private void Update() {
+       body.velocity = new Vector2(Input.GetAxis("Horizontal"), body.velocity.y);
+   }
 }
