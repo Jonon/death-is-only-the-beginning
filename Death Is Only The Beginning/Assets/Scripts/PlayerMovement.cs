bdbc14eca77df0d20bfc7dcc0fa0586fe89bc6d5
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-05-29 11:14:29 +0200

    Add jump animation and add jump once

--- Death Is Only The Beginning/Assets/Scripts/PlayerMovement.cs
+++ Death Is Only The Beginning/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerMovement : MonoBehaviour
    [SerializeField] private float speed;
    private Rigidbody2D body;
    private Animator anim;
+   private bool grounded;
 
    private void Awake() {
        //Grab references for rigidBody and animator object
@@ -22,10 +23,21 @@ public class PlayerMovement : MonoBehaviour
        else if(HorizontalInput < -0.01f)
        transform.localScale = new Vector3(-1, 1, 1);
 
-       if(Input.GetKey(KeyCode.Space)) 
-        body.velocity = new Vector2(body.velocity.x, speed);
+       if(Input.GetKey(KeyCode.Space) && grounded) 
+        Jump();
 
     //Set animator parameter
     anim.SetBool("run", HorizontalInput != 0);
+    anim.SetBool("grounded", grounded);
+   }
+
+   private void Jump() {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        grounded = false;
+   }
+
+   private void OnCollisionEnter2D(Collision2D collision) {
+       if(collision.gameObject.tag == "Ground")
+       grounded = true;
    }
 }
