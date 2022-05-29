b79e7df877c524b4a740631a5e4c14b18fb61796
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-05-29 09:49:45 +0200

    Flip player on left and right movement

--- Death Is Only The Beginning/Assets/Scripts/PlayerMovement.cs
+++ Death Is Only The Beginning/Assets/Scripts/PlayerMovement.cs
@@ -10,7 +10,14 @@ public class PlayerMovement : MonoBehaviour
    }
 
    private void Update() {
-       body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+       float HorizontalInput = Input.GetAxis("Horizontal");
+       body.velocity = new Vector2(HorizontalInput * speed, body.velocity.y);
+
+        // Flip player when moving left or right
+       if(HorizontalInput > 0.01f)
+       transform.localScale = Vector3.one;
+       else if(HorizontalInput < -0.01f)
+       transform.localScale = new Vector3(-1, 1, 1);
 
        if(Input.GetKey(KeyCode.Space)) 
         body.velocity = new Vector2(body.velocity.x, speed);
